110b82d70728c9d7360974aeb99b6fe3164e547b
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-05 15:28:17 -0700

    adds resource routing, user actions, and show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,19 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
   def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :marketing, :articles, :digest)
   end
 end
