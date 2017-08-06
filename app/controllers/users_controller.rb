25682380a33465e1a46a29a54a69ab5fd3d9e0ae
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-06 14:00:35 -0700

    removes current_user and begins check for token

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,26 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    nonce = params[:token]
+    email = params[:email]
+    token = Token.find_by(:nonce => nonce)
+    @user = token.user
+    if @user.email == email
+      @user
+    else
+      render :invalid
+    end
   end
 
   def update
-    if current_user
-      form_params = params.require(:user).permit(user_params)
-      current_user.update_attributes(user_params)
-      redirect_back(fallback_location: root_path)
+    @user = User.find(params[:id])
+    if @user
+      @user.update_attributes(user_params)
       flash[:success] = "Your email preferences are saved!"
+      render :show
     else
-      redirect_back(fallback_location: root_path)
       flash[:alert] = "Oops there was an error when updating your preferences. Try again!"
+      render :show
     end
   end
 
