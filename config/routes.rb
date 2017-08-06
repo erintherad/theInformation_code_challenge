d427aed7e3598911a76030f00971c1e496f2f08a
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-06 14:01:49 -0700

    creates intuitive path for users#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root "users#show"
   resources :users
+
+  get "/users/email-preferences", to: "users#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
