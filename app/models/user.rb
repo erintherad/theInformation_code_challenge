d39b1337dcebb2de2787943581972f85696f9275
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-05 19:11:05 -0700

    adds validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-  validates :email, uniqueness: true
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates_inclusion_of :marketing, in: [true, false]
+  validates_inclusion_of :articles, in: [true, false]
+  validates_inclusion_of :digest, in: [true, false]
 end
