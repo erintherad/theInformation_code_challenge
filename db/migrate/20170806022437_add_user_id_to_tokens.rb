810372153beed2d99ab03a3a36041213034515a1
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-05 19:51:38 -0700

    adds missing migration on token

---
+++ db/migrate/20170806022437_add_user_id_to_tokens.rb
@@ -0,0 +1,5 @@
+class AddUserIdToTokens < ActiveRecord::Migration[5.1]
+  def change
+    add_column :tokens, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170805205734) do
+ActiveRecord::Schema.define(version: 20170806022437) do
 
   create_table "tokens", force: :cascade do |t|
     t.string "nonce"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
