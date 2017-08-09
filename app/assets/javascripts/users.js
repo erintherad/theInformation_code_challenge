cefe3d11d640f2764b66bf82ff87d9519e58f8bb
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-09 15:39:04 -0700

    adds new logic for checkboxes

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,28 @@
-$(function() {
-  //select all checkboxes
-  $("#select_all").change(function(){  //"select all" change
-    $(".checkbox").prop('checked', $(this).prop("checked")); //change all ".checkbox" checked status
+function toggleAll(onOrOff) {
+  $('input.checkbox').each(function() {
+    $(this).prop('checked', onOrOff);
+  });
+}
+
+function updateSelectAll() {
+  var allOn = true;
+
+  $('input.checkbox').each(function() {
+    if(!$(this).prop('checked')) {
+      allOn = false;
+      return; // we found one that is not checked, so we can stop
+    }
   });
 
-  //".checkbox" change
-  $('.checkbox').change(function(){
-    //uncheck "select all", if one of the listed checkbox item is unchecked
-    if(false == $(this).prop("checked")){ //if this item is unchecked
-      $("#select_all").prop('checked', false); //change "select all" checked status to false
-    }
-    //check "select all" if all checkbox items are checked
-    if ($('.checkbox:checked').length == $('.checkbox').length ){
-      $("#select_all").prop('checked', true);
-    }
+  $('#select_all').prop('checked', allOn);
+}
+
+$(document).ready(function() {
+  $('#select_all').on('click', function() {
+    toggleAll($(this).prop('checked'));
   });
-})
+
+  $('input.checkbox').on('click', function() {
+    updateSelectAll();
+  });
+});
