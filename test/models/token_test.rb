b0fda72f7a7648957cdfaa0e20a37359fe3310ff
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-09 14:53:41 -0700

    removes rspec, unnecessary

--- test/models/token_test.rb
+++ test/models/token_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class TokenTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @token = Token.new(nonce: SecureRandom.urlsafe_base64(30), user_id: 1)
+  end
+
+  test "nonce should be present" do
+    @token.nonce = "   "
+    assert_not @token.valid? "The token does not have a nonce."
+  end
+
+  test "nonce and user_id should be unique" do
+    duplicate_nonce = @token.dup
+    @token.save
+    assert_not duplicate_nonce.valid? "The token is not unique."
+  end
 end
