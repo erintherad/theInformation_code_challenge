da65af6c5423c923c8a25d8f71a1e1d078849163
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-09 13:42:20 -0700

    adds notes for clarity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  # Defines current user for a session
   def current_user
     @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
   end
 
+  # Logs in a user
   def login(user)
     if user
       session[:current_user_id] = user.id
     end
   end
 
+  # Logs out a user
   def logout
     session[:current_user_id] = nil
   end
