33b142faa0c0112fd8df8eb35d899e63c9fcca53
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-09 07:43:11 -0700

    creates more specific kinds of seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,32 @@
-5.times do |i|
-  user = User.create(
+3.times do |u|
+  user1 = User.create(
     name: Faker::Name.name,
     email: Faker::Internet.email,
-    marketing: [true, false].sample,
-    articles: [true, false].sample,
-    digest: [true, false].sample
+    marketing: false,
+    articles: false,
+    digest: false
   )
-  Token.generate(user)
+  Token.generate(user1)
+end
+
+3.times do |u|
+  user2 = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    marketing: true,
+    articles: true,
+    digest: true
+  )
+  Token.generate(user2)
+end
+
+3.times do |u|
+  user3 = User.create(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    marketing: false,
+    articles: true,
+    digest: false
+  )
+  Token.generate(user3)
 end
