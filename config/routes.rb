74217afcf206b22707b34c85593199d77cb5153e
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-09 11:32:47 -0700

    adds commented out routes for easy reading

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,16 @@ Rails.application.routes.draw do
 
   get "/admin", to: "admin#index"
   get "/admin/new", to: "admin#new"
+  get "/admin/new", to: "admin#create"
+  get "/generate_token/:id", to: "admin#generate_token", as: "generate_token"
   delete "/admin/:id", to: "admin#delete", as: "delete_user"
 end
+
+# Prefix Verb   URI Pattern                  Controller#Action
+#              root GET    /                            admin#index
+# email_preferences GET    /email-preferences(.:format) users#show
+#                   PUT    /email-preferences(.:format) users#update
+#             admin GET    /admin(.:format)             admin#index
+#    generate_token GET    /admin(.:format)             admin#generate_token
+#         admin_new GET    /admin/new(.:format)         admin#new
+#       delete_user DELETE /admin/:id(.:format)         admin#delete
