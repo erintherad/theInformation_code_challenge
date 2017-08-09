4cc43f92470653cd5ecb7d9ef4a5e3d8665c3e2a
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-09 08:41:22 -0700

    adds logic for select all checkbox

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
-  $('input#all_email_preferences').on('click', function() {
-    $('input:checkbox[name=emailList]').prop('checked', this.checked);
+$( function() {
+  //select all checkboxes
+  $("#select_all").change(function(){  //"select all" change
+      $(".checkbox").prop('checked', $(this).prop("checked")); //change all ".checkbox" checked status
   });
-});
+
+  //".checkbox" change
+  $('.checkbox').change(function(){
+      //uncheck "select all", if one of the listed checkbox item is unchecked
+      if(false == $(this).prop("checked")){ //if this item is unchecked
+          $("#select_all").prop('checked', false); //change "select all" checked status to false
+      }
+      //check "select all" if all checkbox items are checked
+      if ($('.checkbox:checked').length == $('.checkbox').length ){
+          $("#select_all").prop('checked', true);
+      }
+  });
+})
